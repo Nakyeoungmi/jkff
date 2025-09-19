dd2bdfa7e4992ba5a4dc08642f502e566811021d
Nakyeoungmi <qwerty5506a@naver.com>
2025-09-20 01:08:33 +0900

    Create JKFF.v

---
+++ JKFF.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+module JKFF(
+    input wire clk,
+    input wire J,
+    input wire K,
+    output reg Q
+    );
+    always @(posedge clk) begin
+        case({J,K})
+            2'b00:Q<=Q;
+            2'b01:Q<=1'b0;
+            2'b10:Q<=1'b1;
+            2'b11:Q<=~Q;
+        endcase
+    end
+endmodule
